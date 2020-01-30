08689a46ba8801585db55549bfe84c4e18442839
Wanderson Aldo <wanderson.aldo@gmail.com>
2020-01-30 09:19:11 -0200

    Commit base

---
+++ src/components/myCardView.js
@@ -0,0 +1,40 @@
+//This is an example of Card View// 
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Card } from 'react-native-elements';
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Card title="Local Modules">
+        {/*react-native-elements Card*/}
+          <Text style={styles.paragraph}>
+            This is a card from the react-native-elements
+          </Text>
+        </Card>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 40,
+    backgroundColor: '#ecf0f1',
+  },
+
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+
+});
